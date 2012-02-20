f7dfae9f391d1ecba0ceb1e814e1d8a1b1e2313c
Benjamin Eberlei <kontakt@beberlei.de>
2012-02-20 18:04:19 +0100

    Add MIT LICENSE.txt

--- Tests/Deployment/AzureSDKCommandBuilderTest.php
+++ Tests/Deployment/AzureSDKCommandBuilderTest.php
@@ -4,7 +4,7 @@
  *
  * LICENSE
  *
- * This source file is subject to the new BSD license that is bundled
+ * This source file is subject to the MIT license that is bundled
  * with this package in the file LICENSE.txt.
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
