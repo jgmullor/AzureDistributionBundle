1ce5087cca64b32e6613f568a38460ef28eebf0c
Benjamin Eberlei <kontakt@beberlei.de>
2012-03-19 18:13:14 +0100

    Fix GH-3 - Switching E/F drives now and using the Env variable ApplicationPath as default.

--- Resources/role_template/index.php
+++ Resources/role_template/index.php
@@ -1,9 +1,17 @@
 <?php
 
 // Try to find the application root directory.
-$appRoot = "E:\approot\app";
+// Env Variable 'ApplicationPath' is set by the add_environment_variables.ps1
+// script. The others are "default" paths of Azure that can be tried as
+// fallback.
 if (isset($_SERVER['ApplicationPath'])) {
     $appRoot = $_SERVER['ApplicationPath'] . '\app';
+} else if ( file_exists("E:\approot\app")) {
+    $appRoot = "E:\approot\app";
+} else if ( file_exists("F:\approot\app")) {
+    $appRoot = "F:\approot\app";
+} else {
+    exit(1);
 }
 
 require_once $appRoot . '\bootstrap.php.cache';
